ef161fa5df458d049b0a5c1227a3d930a75bf090
Luiz Henrique <https://github.com/luizmorais12/Rockets.git>
2024-08-12 03:08:14 -0300

    feat: ensure bookings details can be viewed

---
+++ @/app/_components/booking-summary.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+// TODO: Create a BookingSummary component that displays the booking summary details.
+const BookingSummary = () => {
+    return <></>
+  }
+  
+  export default BookingSummary
